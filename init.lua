e3a5bbd32f0cb7f97fcaac12fbb673c7f0815ad1
diwang <diwang83963911@gamil.com>
2022-08-10 13:43:08 +0800

    update code

--- init.lua
+++ init.lua
@@ -15,9 +15,35 @@ print(sex,type(sex))
 local coolman
 print(coolman,type(coolman))
 -- function
-function Move(name,place)
+local function Move(name,place)
     return name .. "move to" .. place
 end
 
 local funcResult = Move(name,"mars")
 print(funcResult)
+
+-- TODO:userdata
+
+-- thread
+local co = coroutine.create(function ()
+    print("hello")
+end)
+local result = coroutine.resume(co)
+print(result)
+
+-- table
+local t = {name = "zhangsan",age = 20}
+print(t.name,t.age,type(t))
+
+-- error handling
+local function testError()
+    error("error")
+end
+
+local r,errorMsg = pcall(testError)
+print(r,errorMsg)
+
+r,errorMsg = xpcall(testError,function(err)
+    print("xpcall error:",err)
+end)
+print(r,errorMsg)
